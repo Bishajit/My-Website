043dc8b742c58770d5a4b94ba068bc4c4d763a3a
Bishajit Lodh <Bishajitklodh@gmail.com>
2021-09-24 00:34:13 -0400

    my commit for my personal site

---
+++ test.js
@@ -0,0 +1,27 @@
+//  let person = meg;
+// function greet () {
+//     var name = prompt("What's your name?");
+//     return alert('Hi there ${name}');
+
+// }
+
+// function homeGreet () {
+//     let name = prompt("what is your name?");
+//     return alert(`Hi there ${name}`);
+// }
+
+// let greet2 = () =>{
+//     var person = 'john';
+//     return `hi ${person}`;
+// };
+
+const person = {
+    name='tyrone',
+    age:25,
+    height:'5"9',
+    action: function () {
+        alert('Hello')
+    },
+};
+// alert(greet2());
+// homeGreet();
